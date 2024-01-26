e39c9e34cab1da88cb551b4837735fcf9e8ffb29
Kerlis Gray <k3rl15.g@gmail.com>
2024-01-26 15:26:19 -0500

    Create Button

---
+++ Day 2/youtube_clone/my-stream-app/app/page.tsx
@@ -0,0 +1,14 @@
+// Page.jsx
+import React from 'react';
+import MediaPlayer from '@/components/MediaPlayer';
+import Suggestion from '@/components/Suggestion';
+import Button from '@/components/Button'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <Button />
+    </div>
+  );
+}
